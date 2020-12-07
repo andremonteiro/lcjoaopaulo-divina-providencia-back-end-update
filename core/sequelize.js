b00336355619119f34a4d45fae1f02ec8de51870
DESKTOP-DM4283J\SONG <superstar0428@yandex.com>
2020-12-08 02:48:26 +0900

    Added a Procfile.

--- core/sequelize.js
+++ core/sequelize.js
@@ -1,25 +1,25 @@
 const Sequelize = require('sequelize');
 module.exports = function () {
-    // const config = {
-    //     host: 'divina-database.cuvvtyji1bfq.sa-east-1.rds.amazonaws.com',
-    //     database: 'divina',
-    //     user: 'divina',
-    //     pass: 'divina123',
-    //     adapter: 'postgres',
-    //     reconnect: true,
-    //     timezone: '-03:00',
-    //     logging: console.log
-    // };
     const config = {
-        host: 'localhost',
-        database: 'divina-providencia',
-        user: 'postgres',
-        pass: 'adminpwd',
+        host: 'divina-database.cuvvtyji1bfq.sa-east-1.rds.amazonaws.com',
+        database: 'divina',
+        user: 'divina',
+        pass: 'divina123',
         adapter: 'postgres',
         reconnect: true,
         timezone: '-03:00',
         logging: console.log
     };
+    // const config = {
+    //     host: 'localhost',
+    //     database: 'divina-providencia',
+    //     user: 'postgres',
+    //     pass: 'adminpwd',
+    //     adapter: 'postgres',
+    //     reconnect: true,
+    //     timezone: '-03:00',
+    //     logging: console.log
+    // };
     const connector = new Sequelize(config.database, config.user, config.pass, {
         host: config.host,
         dialect: config.adapter,
